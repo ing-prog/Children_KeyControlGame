0473ce895b060c7158f3b7e065f109f12f9a0422
maxim <maxim218maxim218@yandex.ru>
2018-06-06 07:55:22 +0300

    Add timers

--- gameCode.js
+++ gameCode.js
@@ -5,19 +5,43 @@ window.onload = function() {
     let startGameBtn = document.getElementById("startGameBtn");
     let gameContentBox = document.getElementById("gameContentBox");
     let keyLabel = document.getElementById("keyLabel");
+    let timeLabel = document.getElementById("timeLabel");
 
     // was button clicked
     let buttonPressed = false;
 
+    // time of game
+    let nowTime = 0;
+    // max time of game
+    let maxTime = 30;
+
     // start repeating function
     startGameBtn.onclick = function() {
+        // if user did not push button before
         if(buttonPressed === false) {
+            // make button pushed
             buttonPressed = true;
+
+            // hide button
             startGameBtn.hidden = true;
+            // show game box
             gameContentBox.hidden = false;
+
+            // start game interval
             let gameInterval = setInterval(function() {
                 gameProcess();
             }, 50);
+
+            // start seconds interval count
+            let secondsInterval = setInterval(function() {
+                nowTime++;
+                timeLabel.innerHTML = "Осталось: " + (maxTime - nowTime);
+                if(nowTime === maxTime) {
+                    // clear timers
+                    clearInterval(gameInterval);
+                    clearInterval(secondsInterval);
+                }
+            }, 1000);
         }
     };
 
