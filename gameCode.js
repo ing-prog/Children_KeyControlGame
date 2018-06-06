66f53e2b7ec043eebffdbe48633aa4ff5d5e80b6
maxim <maxim218maxim218@yandex.ru>
2018-06-06 07:28:53 +0300

    Add button, key, interval events

--- gameCode.js
+++ gameCode.js
@@ -1,7 +1,38 @@
 "use strict";
 
 window.onload = function() {
+    // get elements from web page
+    let startGameBtn = document.getElementById("startGameBtn");
+    let gameContentBox = document.getElementById("gameContentBox");
     let keyLabel = document.getElementById("keyLabel");
 
-    
+    // was button clicked
+    let buttonPressed = false;
+
+    // start repeating function
+    startGameBtn.onclick = function() {
+        if(buttonPressed === false) {
+            buttonPressed = true;
+            startGameBtn.hidden = true;
+            gameContentBox.hidden = false;
+            let gameInterval = setInterval(function() {
+                gameProcess();
+            }, 50);
+        }
+    };
+
+    // game function
+    function gameProcess() {
+
+    }
+
+    // add key down event
+    window.onkeydown = function(event) {
+
+    };
+
+    // add key up event
+    window.onkeyup = function(event) {
+
+    };
 };
